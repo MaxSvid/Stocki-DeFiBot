d2c2f933822cca440eabce8067c2cdec20e5f4b0
MaksSvid <sviderskiymask04@gmail.com>
2025-06-26 19:17:47 +0100

    Remove data/ directory

---
+++ bot/database/db.py
@@ -0,0 +1,7 @@
+import psycopg
+from psycopg import OperationalError
+from dotenv import load_dotenv
+import os
+
+# Loading 
+load_dotenv()
---
+++ init_DB/init.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS payments;
+
+-- USERS TABLE
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    telegram_id BIGINT UNIQUE NOT NULL,
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT
+);
+
+-- PAYMENTS TABLE
+CREATE TABLE payments (
+    payment_id SERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    amount NUMERIC(10, 2),
+    status TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
