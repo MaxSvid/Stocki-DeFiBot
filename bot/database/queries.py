9deac8a4ef1fbab2cc1f32040019a137a1cb89b5
Max Sviderskyi <148738111+MaxSvid@users.noreply.github.com>
2025-06-29 00:28:39 +0100

    Update queries.py

--- bot/database/queries.py
+++ bot/database/queries.py
@@ -1,4 +1,4 @@
 """
 It is best to use pgadmin for queries, I created it on localhost 5050:80
-Still good idea to use queries.py or docker-compose exec psql -U POSTGRES_USER -d POSTGRES_DB
+Still good idea to use queries.py or docker-compose exec postgres psql -U POSTGRES_USER -d POSTGRES_DB
 """
